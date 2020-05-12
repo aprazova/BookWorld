90781a3c3f6924a7bf37c53ee2514c711a383b73
aprazova <alexandra.prazova@gmail.com>
2020-05-12 15:32:49 +0300

    Restucturing files tree

--- js/changeButton.js
+++ js/changeButton.js
@@ -1,14 +1,15 @@
-document.getElementById("add-fa").addEventListener(
-    "click",
-    function(event) {
-        if (event.target.value === "Add to Favourite Authors") {
-            event.target.value = "Remove from Favourite Authors";
-        } else {
-            event.target.value = "Add to Favourite Authors";
-        }
-    },
-    false
-);
+// document.getElementById("add-fa").addEventListener(
+//     "click",
+//     function(event) {
+//         if (event.target.value === "Add to Favourite Authors") {
+//             event.target.value = "Remove from Favourite Authors";
+//         } else {
+//             event.target.value = "Add to Favourite Authors";
+//         }
+//     },
+//     false
+// );
+
 document.getElementById("add-fa-0").addEventListener(
     "click",
     function(event) {
